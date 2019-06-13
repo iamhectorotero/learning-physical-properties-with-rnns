623f3008cbca237718cecf1aef8ba6e5f6de1e43
Héctor Otero <7hector2@gmail.com>
2019-06-13 12:31:41 +0100

    YOKED TRAINING CONSTANTS

--- isaac/constants.py
+++ isaac/constants.py
@@ -1,4 +1,4 @@
 BASIC_TRAINING_COLS = [o+"."+attr for o in ["o1", "o2", "o3", "o4"] for attr in ["x", "y", "vx", "vy"]]
-YOKED_TRAINING_COLS = BASIC_TRAINING_COLS + ["idControlledObject", "mouseX", "mouseY"]
+YOKED_TRAINING_COLS = BASIC_TRAINING_COLS + ["C_none", "C_O1", "C_O2", "C_O3", "C_O4", "mouseX", "mouseY"]
 FORCE_CLASS_COLS = ["attract", "none", "repel"]
 MASS_CLASS_COLS = ["A", "B", "same"]
