6736d606f7e7ef8b7e0df054fe31ba611cbd28d8
Héctor Otero <7hector2@gmail.com>
2019-06-27 17:38:59 +0100

    Add dropout before fully connected layer

--- isaac/models.py
+++ isaac/models.py
@@ -2,40 +2,20 @@ import torch.nn as nn
 import numpy as np
 import torch
 
-class RNNModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
-        super(RNNModel, self).__init__()
-        # Number of hidden dimensions
-        self.hidden_dim = hidden_dim
-        
-        # Number of hidden layers
-        self.layer_dim = layer_dim
-        
-        # RNN
-        self.rnn = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
-        
-        # Readout layer
-        self.fc = nn.Linear(hidden_dim, output_dim)
-    
-    def forward(self, x):
-        out, _ = self.rnn(x)
-        out = self.fc(out[:, -1, :]) 
-        return out
-    
-    
+
 class ComplexRNNModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, n_layers, output_dim, dropout=0.):
         super(ComplexRNNModel, self).__init__()
         # RNN
-        self.rec_one = nn.GRU(input_dim, hidden_dim, n_layers // 2, batch_first=True, dropout=dropout)
-        self.rec_two = nn.GRU(hidden_dim, hidden_dim, n_layers // 2, batch_first=True, dropout=dropout)
+        self.rec_layer = nn.GRU(input_dim, hidden_dim, n_layers, batch_first=True, dropout=dropout)
         # Readout layer
+        self.dropout = nn.Dropout(p=dropout/2)
         self.fc = nn.Linear(hidden_dim, output_dim)
     
     def forward(self, x):
-        out, _ = self.rec_one(x)
-        out, _ = self.rec_two(out)
-        out = self.fc(out[:, -1, :]) 
+        out, _ = self.rec_layer(x)
+        out = self.dropout(out[:, -1, :])
+        out = self.fc(out) 
         return out
     
     
