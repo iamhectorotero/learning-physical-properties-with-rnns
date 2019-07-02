6922851abd57b6db9653ebc4b0c8481a18adffdb
Héctor Otero <7hector2@gmail.com>
2019-07-02 16:58:38 +0100

    Environment set-up first steps

---
+++ toddler/action_coding.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+NO_OP = 0
+ACCELERATE_IN_X = 1
+ACCELERATE_IN_Y = 2
+DECELERATE_IN_X = 3
+DECELERATE_IN_Y = 4
+CLICK = 5
+FIRST_ANSWER = 6
+SECOND_ANSWER = 7
+THIRD_ANSWER = 8
+MAX_X = 6.0
+MAX_Y = 4.0
+MIN_X = MIN_Y = 0.0
+TIME_CONSTANT = 1./60
+
+mass_answers = {FIRST_ANSWER:"A is heavier", SECOND_ANSWER:"B is heavier", THIRD_ANSWER:"same"}
+force_answers = {FIRST_ANSWER:"attract", SECOND_ANSWER:"repel", THIRD_ANSWER:"none"}
+
+def update_velocity(vx, vy, action):
+    velocity_decrease = 2
+    velocity_increment = 3
+    
+    if action == NO_OP:
+        vx /= velocity_decrease
+        vy /= velocity_decrease
+    elif action == ACCELERATE_IN_X:
+        vx += velocity_increment
+    elif action == ACCELERATE_IN_Y:
+        vy += velocity_increment
+    elif action == DECELERATE_IN_X:
+        vx -= velocity_increment
+    elif action == DECELERATE_IN_Y:
+        vy -= velocity_increment
+    if action == CLICK:
+        vx /= velocity_decrease
+        vy /= velocity_decrease
+    return vx, vy
+
+def update_position(pos, vel, max_pos, min_pos):
+    
+    pos += vel * TIME_CONSTANT
+    if pos > max_pos:
+        pos = max_pos
+        vel = 0
+    if pos < min_pos:
+        pos = min_pos
+        vel = 0
+            
+    return pos, vel
+
+def get_mouse_action(action, x_pos, y_pos, vx, vy, mouse_click, object_in_control):
+    vx, vy = update_velocity(vx, vy, action)
+    x_pos, vx = update_position(x_pos, vx, MAX_X, MIN_X)
+    y_pos, vy = update_position(y_pos, vy, MAX_Y, MIN_Y)
+    
+    click = (action == CLICK)
+    if object_in_control != 0:
+        click = np.random.rand() > 0.2
+
+    return click, x_pos, y_pos, vx, vy
