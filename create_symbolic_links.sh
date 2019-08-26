14f7295a2b543e5a06ae0f33144c93c34f453478
Héctor Otero <7hector2@gmail.com>
2019-08-26 22:36:58 +0100

    Add script to create symbolic links

---
+++ create_symbolic_links.sh
@@ -0,0 +1,6 @@
+rm toddler/simulator diss_results/toddler diss_results/simulator diss_results/isaac toddler/isaac
+ln -s $PWD/isaac toddler
+ln -s $PWD/simulator toddler
+ln -s $PWD/toddler diss_results
+ln -s $PWD/simulator diss_results
+ln -s $PWD/isaac diss_results
