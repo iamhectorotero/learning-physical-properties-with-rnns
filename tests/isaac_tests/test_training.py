c86ce0781c06a5e7b58a65e4d2b836b1d58efd96
Héctor Otero <7hector2@gmail.com>
2019-09-27 14:29:47 +0200

    Add tests to evaluate_saved_model

--- tests/isaac_tests/test_training.py
+++ tests/isaac_tests/test_training.py
@@ -1,9 +1,16 @@
 import unittest
 import numpy as np
+import pandas as pd
 import torch
+import os
+import joblib
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.exceptions import NotFittedError
 
 from isaac import training
-from isaac.models import initialise_model
+from isaac.models import initialise_model, ComplexRNNModel
+from isaac.dataset import normalise
 
 class TestEvaluate(unittest.TestCase):
     @staticmethod
@@ -186,6 +193,152 @@ class TestEvaluate(unittest.TestCase):
         training.evaluate(model, val_loader, return_predicted,
                           seq_start, seq_end, step_size)
 
+
+class TestEvaluateSavedModel(unittest.TestCase):
+    model_path = "temporary_model"
+    dataset_path = "temporary_dataset.h5"
+    scaler_path = "temporary_scaler"
+    plot_path = "temporary_plot.pdf"
+
+    n_trials = 10
+    n_features = 5
+    training_columns = ["feature_" + str(i) for i in range(n_features)]
+    n_classes = 3
+    class_columns = ["class_" + str(i) for i in range(n_classes)]
+
+    hidden_dim = 5
+    n_layers = 1
+    network_params = (n_features, hidden_dim, n_layers, n_classes)
+
+    def tearDown(self):
+        for temp_file in [self.model_path, self.dataset_path, self.scaler_path, self.plot_path]:
+            if os.path.exists(temp_file):
+                os.remove(temp_file)
+
+    @staticmethod
+    def write_dataframes_to_file(dataframes, path):
+        for i, dataframe in enumerate(dataframes):
+            dataframe.to_hdf(path, key="key_"+str(i))
+
+    @staticmethod
+    def create_dataset(n_trials, training_cols, class_cols, multiclass=False, seed=0):
+        np.random.seed(seed)
+
+        dfs = []
+        seq_length = np.random.randint(25, 100)
+
+        for _ in range(n_trials):
+
+            if not multiclass:
+                data = np.random.rand(seq_length, len(training_cols) + len(class_cols))
+                classes = np.random.rand(len(class_cols))
+                classes = (classes == max(classes))
+
+                df = pd.DataFrame(data, columns=training_cols + class_cols)
+                df[class_cols] = classes
+
+            if multiclass:
+                all_class_cols = list(np.concatenate(class_cols))
+                data = np.random.rand(seq_length, len(training_cols) + len(all_class_cols))
+                df = pd.DataFrame(data, columns=training_cols + all_class_cols)
+
+                for class_i_cols in class_cols:
+                    classes = np.random.rand(len(class_i_cols))
+                    classes = (classes == max(classes))
+                    df[class_i_cols] = classes
+
+            dfs.append(df)
+
+        return dfs
+
+    @staticmethod
+    def create_and_save_model(network_params, model_path, arch=ComplexRNNModel):
+        model, _, _ = initialise_model(network_params, arch=arch)
+        torch.save(model.state_dict(), model_path)
+
+    def test_existing_dataset_path(self):
+        self.create_and_save_model(self.network_params, self.model_path)
+        dataset = self.create_dataset(self.n_trials, self.training_columns, self.class_columns)
+        self.write_dataframes_to_file(dataset, self.dataset_path)
+
+        training.evaluate_saved_model(self.model_path, self.network_params, self.dataset_path,
+                                      self.training_columns, self.class_columns, trials=None)
+
+    def test_inexisting_dataset_path(self):
+        self.create_and_save_model(self.network_params, self.model_path)
+
+        with self.assertRaises(OSError):
+            training.evaluate_saved_model(self.model_path, self.network_params, "invented_path.h5",
+                                          self.training_columns, self.class_columns)
+
+    def test_inexisting_path_and_trials_not_none(self):
+        self.create_and_save_model(self.network_params, self.model_path)
+        trials = self.create_dataset(self.n_trials, self.training_columns, self.class_columns)
+
+        training.evaluate_saved_model(self.model_path, self.network_params, "invented_path.h5",
+                                      self.training_columns, self.class_columns, trials=trials)
+
+    def test_scaler_path_is_not_none(self):
+        self.create_and_save_model(self.network_params, self.model_path)
+        trials = self.create_dataset(self.n_trials, self.training_columns, self.class_columns)
+
+        X = np.array([np.array(trial[self.training_columns]) for trial in trials])
+        X = X.reshape(-1, self.n_features)
+        scaler = StandardScaler()
+        scaler.fit(X)
+        joblib.dump(scaler, self.scaler_path)
+
+        training.evaluate_saved_model(self.model_path, self.network_params, "invented_path.h5",
+                                      self.training_columns, self.class_columns, trials=trials,
+                                      scaler_path=self.scaler_path)
+
+    def test_unfitted_scaler(self):
+        self.create_and_save_model(self.network_params, self.model_path)
+        trials = self.create_dataset(self.n_trials, self.training_columns, self.class_columns)
+
+        scaler = StandardScaler()
+        joblib.dump(scaler, self.scaler_path)
+
+        with self.assertRaises(NotFittedError):
+            training.evaluate_saved_model(self.model_path, self.network_params, None,
+                                          self.training_columns, self.class_columns, trials=trials,
+                                          scaler_path=self.scaler_path)
+
+    def test_incorrect_architecture(self):
+        class AlternativeArch(torch.nn.Module):
+            def __init__(self, cell_type):
+                super(AlternativeArch, self).__init__()
+                self.rec_layer = cell_type(2, 2, 2, False)
+            def forward(self, x):
+                return self.rec_layer(x)[0]
+
+        self.create_and_save_model((), self.model_path, arch=AlternativeArch)
+        trials = self.create_dataset(self.n_trials, self.training_columns, self.class_columns)
+
+        with self.assertRaises(RuntimeError):
+            training.evaluate_saved_model(self.model_path, self.network_params, None,
+                                          self.training_columns, self.class_columns, trials=trials)
+
+    def test_incorrect_network_params(self):
+        wrong_params = (4, 10, 2, 3)
+        self.create_and_save_model(self.network_params, self.model_path)
+        dataset = self.create_dataset(self.n_trials, self.training_columns, self.class_columns)
+        self.write_dataframes_to_file(dataset, self.dataset_path)
+
+        with self.assertRaises(RuntimeError):
+            training.evaluate_saved_model(self.model_path, wrong_params, self.dataset_path,
+                                          self.training_columns, self.class_columns, trials=None)
+
+    def test_save_plot(self):
+        self.create_and_save_model(self.network_params, self.model_path)
+        dataset = self.create_dataset(self.n_trials, self.training_columns, self.class_columns)
+        self.write_dataframes_to_file(dataset, self.dataset_path)
+
+        training.evaluate_saved_model(self.model_path, self.network_params, self.dataset_path,
+                                      self.training_columns, self.class_columns, trials=None,
+                                      save_plot_path=self.plot_path)
+
+        self.assertTrue(os.path.exists(self.plot_path))
 
 if __name__ == "__main__":
     unittest.main()
