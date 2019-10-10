2e422fb2010506af21e89add4faf32d33eac885d
Héctor Otero <7hector2@gmail.com>
2019-10-10 14:00:31 +0200

    Add testing for training_loop

--- tests/isaac_tests/test_training.py
+++ tests/isaac_tests/test_training.py
@@ -1,12 +1,18 @@
 import unittest
+import unittest.mock
 import numpy as np
 import pandas as pd
 import torch
 import os
 import joblib
+import io
+import sys
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.exceptions import NotFittedError
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from copy import deepcopy
 
 from isaac import training
 from isaac.models import initialise_model, ComplexRNNModel
@@ -339,6 +345,370 @@ class TestEvaluateSavedModel(unittest.TestCase):
                                       save_plot_path=self.plot_path)
 
         self.assertTrue(os.path.exists(self.plot_path))
+
+
+class TestTrainingLoop(unittest.TestCase):
+    def tearDown(self):
+        sys.stderr = sys.__stderr__
+
+    @staticmethod
+    def create_loader(n_features, n_classes, n_examples=10, seq_length=100):
+
+        X = np.random.rand(n_examples, seq_length, n_features).astype('f')
+        Y = np.random.randint(low=0, high=n_classes, size=n_examples)
+
+        X = torch.from_numpy(X)
+        Y = torch.from_numpy(Y)
+
+        tensor_dataset = torch.utils.data.TensorDataset(X, Y)
+        val_loader = torch.utils.data.DataLoader(tensor_dataset, shuffle=False)
+
+        return val_loader
+
+    @staticmethod
+    def create_model(n_features, n_classes):
+        hidden_dim = 5
+        n_layers = 1
+        network_params = (n_features, hidden_dim, n_layers, n_classes)
+        model, _, _ = initialise_model(network_params)
+        return model
+
+    @staticmethod
+    def get_max_epochs_without_improvement(accuracies):
+        max_epochs_without_improvement = 0
+        epochs_without_improvement = 0
+        best_accuracy = 0.
+
+        for acc in accuracies:
+            if acc > best_accuracy:
+                best_accuracy = acc
+                max_epochs_without_improvement = max(max_epochs_without_improvement,
+                                                     epochs_without_improvement)
+                epochs_without_improvement = 0
+            else:
+                epochs_without_improvement += 1
+
+        max_epochs_without_improvement = max(max_epochs_without_improvement,
+                                             epochs_without_improvement)
+
+        return max_epochs_without_improvement
+
+    def test_positive_patience(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 10
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = 2
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+        max_epochs_without_improvement = self.get_max_epochs_without_improvement(accuracies[1])
+        # The training stops when the number of epochs without improvement surpasses the patience
+        self.assertGreaterEqual(patience + 1, max_epochs_without_improvement)
+
+    def test_zero_patience(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 10
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = 0
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+        max_epochs_without_improvement = self.get_max_epochs_without_improvement(accuracies[1])
+        # The first time the accuracy doesn't improve the training stops
+        self.assertGreaterEqual(1, max_epochs_without_improvement)
+
+    def test_negative_patience(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 10
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = -2
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+        max_epochs_without_improvement = self.get_max_epochs_without_improvement(accuracies[1])
+        # Negative patience behaves equal to zero patience
+        self.assertGreaterEqual(1, max_epochs_without_improvement)
+
+    def test_positive_num_epochs(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 10
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        self.assertEqual(len(losses), num_epochs)
+        self.assertEqual(len(accuracies[0]), num_epochs)
+        self.assertEqual(len(accuracies[1]), num_epochs)
+
+    def test_zero_num_epochs(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 0
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        self.assertEqual(len(losses), num_epochs)
+        self.assertEqual(len(accuracies[0]), num_epochs)
+        self.assertEqual(len(accuracies[1]), num_epochs)
+
+    def test_negative_num_epochs(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = -1
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        # Negative epochs behaves like 0 epochs
+        self.assertEqual(len(losses), 0)
+        self.assertEqual(len(accuracies[0]), 0)
+        self.assertEqual(len(accuracies[1]), 0)
+
+    def test_model_is_fitted(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        initial_model = deepcopy(model)
+
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 10
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        # Assumes the accuracy of the model will improve
+        for p1, p2 in zip(initial_model.parameters(), best_model.parameters()):
+            self.assertFalse(torch.eq(p1.data, p2.data).any())
+
+    def test_error_improves_with_only_one_class_in_the_training_set(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+
+        # Setting a small learning rate to ensure that improvement happens over the span
+        # of multiple epochs
+        optimizer = Adam(model.parameters(), lr=1e-8)
+        error = CrossEntropyLoss()
+
+        # Setting the number of training classes to 1, overfitting is easy and error should
+        # necessarily improve
+        train_loader = self.create_loader(n_features, 1)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 10
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        self.assertNotEqual(losses[0], min(losses))
+
+    def test_error_improves_with_more_than_one_class_in_the_training_set(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        # Even with classes randomly assigned, the model should be able to overfit to the training
+        # set even if this doesn't imply improvement in the validation
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 10
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        self.assertNotEqual(losses[0], min(losses))
+        # Accuracy in the training set should be better than random
+        self.assertGreaterEqual(max(accuracies[0]), 1./n_classes)
+
+    def test_model_with_best_val_accuracy_is_returned(self):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        initial_model = deepcopy(model)
+
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 10
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        best_model_accuracy = training.evaluate(best_model, val_loader)
+        self.assertEqual(max(accuracies[1]), best_model_accuracy)
+
+    @unittest.mock.patch('sys.stderr', new_callable=io.StringIO)
+    def test_print_stats_per_epoch_is_true(self, mock_stderr):
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        initial_model = deepcopy(model)
+
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 1
+        print_stats_per_epoch = True
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        # Stats are printed once more at the end of the loop
+        self.assertEqual(mock_stderr.getvalue().count("Train_loss"), num_epochs + 1)
+        self.assertEqual(mock_stderr.getvalue().count("Train_acc"), num_epochs + 1)
+        self.assertEqual(mock_stderr.getvalue().count("Val_acc"), num_epochs + 1)
+
+
+    @unittest.mock.patch('sys.stderr', new_callable=io.StringIO)
+    def test_print_stats_per_epoch_is_false(self, mock_stderr):
+        sys.stderr = sys.__stderr__
+        n_features = 4
+        n_classes = 3
+        model = self.create_model(n_features, n_classes)
+        initial_model = deepcopy(model)
+
+        optimizer = Adam(model.parameters())
+        error = CrossEntropyLoss()
+
+        train_loader = self.create_loader(n_features, n_classes)
+        val_loader = self.create_loader(n_features, n_classes)
+        num_epochs = 1
+        print_stats_per_epoch = False
+
+        seq_start, seq_end, seq_step = None, None, None
+        patience = np.inf
+
+        losses, accuracies, best_model = training.training_loop(model, optimizer, error,
+                                                                train_loader, val_loader,
+                                                                num_epochs, print_stats_per_epoch,
+                                                                seq_start, seq_end, seq_step,
+                                                                patience)
+
+        self.assertEqual(mock_stderr.getvalue().count("Train_loss"), 0)
+        self.assertEqual(mock_stderr.getvalue().count("Train_acc"), 0)
+        self.assertEqual(mock_stderr.getvalue().count("Val_acc"), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
