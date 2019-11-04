8b2ff7992e5f3835d929e10b6fb8c060531e9a7a
Héctor <7hector2@gmail.com>
2019-11-04 13:23:36 +0100

    Update setup.py

--- libraries/setup.py
+++ libraries/setup.py
@@ -4,18 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="diss-iamhectorotero",
-    version="0.0.1",
+    name="learning-physical-properties-with-rnns-iamhectorotero",
+    version="1.0.0",
     author="Hector Otero",
     author_email="7hector2@gmail.com",
     description="A package to train RNN in physical microworlds in a supervised or RL manner",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/iamhectorotero/diss",
+    url="https://github.com/iamhectorotero/learning-physical-properties-with-rnns",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5.6',
