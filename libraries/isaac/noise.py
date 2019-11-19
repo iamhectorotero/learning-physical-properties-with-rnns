dd17d1ffa979ec6cb0678148f6c700b87d5e51af
Héctor Otero <7hector2@gmail.com>
2019-11-19 12:46:10 +0100

    Add documentation and specify batch size in add_noise_to_dataloader

--- libraries/isaac/noise.py
+++ libraries/isaac/noise.py
@@ -3,6 +3,14 @@ import torch.utils.data
 from torch.distributions import Normal
 
 def add_noise_to_dataloader(dataloader, noise_deviation=0.1, seed=0):
+    """Adds Gaussian noise with mean 0 to the dataloader.
+    Args:
+        dataloader: a torch DataLoader.
+        noise_deviation: (default=0.1) The standard deviation of the Gaussian distribution.
+        seed: the value that will be set to the torch rng before generating the noise values.
+    Returns:
+        dataloader_with_noise"""
+
     torch.manual_seed(seed)
 
     X, Y = dataloader.dataset.tensors
@@ -11,6 +19,7 @@ def add_noise_to_dataloader(dataloader, noise_deviation=0.1, seed=0):
     X_with_noise = X + noise
 
     tensor_dataset = torch.utils.data.TensorDataset(X_with_noise, Y)
-    dataloader_with_noise = torch.utils.data.DataLoader(tensor_dataset, shuffle=False)
+    dataloader_with_noise = torch.utils.data.DataLoader(tensor_dataset, shuffle=False,
+                                                        batch_size=dataloader.batch_size)
 
     return dataloader_with_noise
