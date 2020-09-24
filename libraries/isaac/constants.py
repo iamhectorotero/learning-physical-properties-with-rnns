fa5fb8fd4540652f3eabef0015ff3472eb1a7f18
Héctor Otero <7hector2@gmail.com>
2020-09-24 12:26:14 +0200

    Add R and theta feature combinations

--- libraries/isaac/constants.py
+++ libraries/isaac/constants.py
@@ -1,6 +1,9 @@
 POSITION_COLS = tuple([o+"."+attr for o in ["o1", "o2", "o3", "o4"] for attr in ["x", "y"]])
-BASIC_TRAINING_COLS = tuple([o+"."+attr for o in ["o1", "o2", "o3", "o4"] for attr in ["x", "y", "vx", "vy"]])
-ALT_BASIC_TRAINING_COLS = tuple([o+"."+attr for o in ["o1", "o2", "o3", "o4"] for attr in ["x", "y", "speed", "angle"]])
+VELOCITY_COLS = tuple([o+"."+attr for o in ["o1", "o2", "o3", "o4"] for attr in ["vx", "vy"]])
+BASIC_TRAINING_COLS = POSITION_COLS + VELOCITY_COLS
+RTHETA_COLS = tuple([o+"."+attr for o in ["o1", "o2", "o3", "o4"] for attr in ["r", "theta"]])
+XY_RTHETA_COLS = POSITION_COLS + RTHETA_COLS
+XY_VXVY_RTHETA_COLS = BASIC_TRAINING_COLS + RTHETA_COLS
 PUCK_DISTANCES = ('d_o1_o2', 'd_o1_o3', 'd_o1_o4', 'd_o2_o3', 'd_o2_o4', 'd_o3_o4')
 PUCK_SQUARE_DISTANCES = ('d2_o1_o2', 'd2_o1_o3', 'd2_o1_o4', 'd2_o2_o3', 'd2_o2_o4', 'd2_o3_o4')
 PUCK_ANGLE_FEATURES = ('cos_o1_o2', 'sin_o1_o2', 'cos_o1_o3', 'sin_o1_o3', 'cos_o1_o4', 'sin_o1_o4', 
